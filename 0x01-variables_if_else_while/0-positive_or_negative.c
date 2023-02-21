63af4ae6987ccfbe6a421d3c579d998cd2b80b6d
my username <my email.com>
2023-02-20 19:09:14 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,6 +11,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is postive\n", n);
@@ -19,7 +20,7 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n <0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
