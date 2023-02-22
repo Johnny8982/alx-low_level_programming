fdbfe6174362186e5b56b139324e012a1c54044f
my username <my email.com>
2023-02-22 13:11:12 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,21 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
-#include <stdio.h>
 int main(void)
 {
-int r;
+		int r;
 
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+		r = _abs(-1);
+		printf("%d\n", r);
+		r =_abs(0);
+		printf("%d\n", r);
+		r =_abs(1);
+		printf("%d\n", r);
+		r =_abs(-98);
+		printf("%d\n", r);
+		return (0);
 }
