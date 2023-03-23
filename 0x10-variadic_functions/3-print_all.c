d75951cb085a5e1c302c0139e28e864663b0615e
my username <my email.com>
2023-03-23 16:04:16 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,11 +9,11 @@ void print_all(const char * const format, ...)
 {
 		int i = 0;
 		char *str, *sep = "";
-		
+
 		va_list list;
 
 		va_start(list, format);
-		
+
 		if (format)
 		{
 			while (format[i])
@@ -30,7 +30,6 @@ void print_all(const char * const format, ...)
 						break;
 					case 's':
 						str = va_arg(list, char *);
-						
 						if (!str)
 							str = "(nil)";
 						printf("%s%s", sep, str);
